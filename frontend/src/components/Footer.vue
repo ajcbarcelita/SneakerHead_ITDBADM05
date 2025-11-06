fffdb0c892bc6b55284bc7f5d2406d7a3df66474
DouiJ <141159120+DouiJ@users.noreply.github.com>
2025-11-07 02:01:45 +0800

    Footer and NavBar components added

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-white-smoke text-gray font-Montserrat">
+    <div class="container mx-auto px-3 py-3 flex flex-col md:flex-row items-center justify-between">
+      
+      <div class="flex items-center gap-3">
+        <router-link to="/" class="flex items-center gap-3">
+          <img src="@/assets/FooterLogo.png" alt="SneakerHead" class="h-16 w-auto" />
+          <span class="font-semibold text-oxford-blue">SneakerHead</span>
+        </router-link>
+        <a href="mailto:A3J@sneakerhead.ph" class="text-sm text-oxford-blue hover:underline">A3J@sneakerhead.ph</a>
+      </div>
+
+      <div class="mt-4 md:mt-0 text-sm text-gray"> 2025 © SNEAKERHEAD.PH. ALL RIGHTS RESERVED.</div>
+    </div>
+  </footer>
+</template>
