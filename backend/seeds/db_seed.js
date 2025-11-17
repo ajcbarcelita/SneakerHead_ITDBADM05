d3a3331bb9400f58e7618c2519307b534d08420f
ajcbarcelita <aaron_barcelita@dlsu.edu.ph>
2025-11-17 15:39:36 +0800

    Temporarily removed order inserts, since new schema.

--- backend/seeds/db_seed.js
+++ backend/seeds/db_seed.js
@@ -46,8 +46,8 @@ async function runSeeds() {
       "sneakerhead_province_inserts.sql",
       "sneakerhead_cities_municipalities_insert.sql",
       "sneakerhead_inserts.sql",
-      "promo_codes_inserts.sql",
-      "orders_inserts.sql",
+      "promo_codes_inserts.sql"
+      // add more files as needed
     ];
 
     for (const file of sqlFiles) {
